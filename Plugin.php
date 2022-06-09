a463436a2b47a2ca561130eca95440f8562b3427
gogobody <138950128@qq.com>
2022-06-09 14:58:53 +0800

    推迟检测更新

--- Plugin.php
+++ Plugin.php
@@ -9,8 +9,8 @@ use Widget\Options;
 
 /**
  * jkOptionsFramework 是 Typecho 第一款选项框架
- * <div class="jkOptionsStyle"><a style="width:fit-content" id="jkOptions">版本检测中..</div>&nbsp;</div><style>.jkOptionsStyle{margin-top: 5px;}.jkOptionsStyle a{font-size: smaller;background: #4DABFF;padding: 5px;color: #fff;}</style>
- * <script>var simversion="1.0.0";function update_detec(){var container=document.getElementById("jkOptions");if(!container){return}var ajax=new XMLHttpRequest();container.style.display="block";ajax.open("get","https://api.github.com/repos/gogobody/jkOptionsFramework/releases/latest");ajax.send();ajax.onreadystatechange=function(){if(ajax.readyState===4&&ajax.status===200){var obj=JSON.parse(ajax.responseText);var newest=obj.name;if(newest>simversion){container.innerHTML="发现新版本："+obj.name+'。下载地址：<a href="'+obj.zipball_url+'">点击下载</a>'+"<br>您目前的版本:"+String(simversion)+"。"}else{container.innerHTML="您目前使用的是最新版。"}}}};update_detec();</script>
+ * <div class="jkOptionsStyle"><a style="width:fit-content" id="jkOptions">您目前使用的是最新版</div>&nbsp;</div><style>.jkOptionsStyle{margin-top: 5px;}.jkOptionsStyle a{font-size: smaller;background: #4DABFF;padding: 5px;color: #fff;}</style>
+ * <script>var jkoptversion="1.0.0";function jkoptionsframework_update(){localStorage.setItem("jkoptionsframework_update",(new Date()).getHours());var container=document.getElementById("jkOptions");if(!container){return}var ajax=new XMLHttpRequest();container.style.display="block";ajax.open("get","https://api.github.com/repos/gogobody/jkOptionsFramework/releases/latest");ajax.send();ajax.onreadystatechange=function(){if(ajax.readyState===4&&ajax.status===200){var obj=JSON.parse(ajax.responseText);var newest=obj.name;if(newest>jkoptversion){container.innerHTML="发现新版本："+obj.name+'。下载地址：<a href="'+obj.zipball_url+'">点击下载</a>'+"<br>您目前的版本:"+String(jkoptversion)+"。"}else{container.innerHTML="您目前使用的是最新版。"}}}};if(Math.abs(localStorage.getItem("jkoptionsframework_update") - (new Date()).getHours())>1){jkoptionsframework_update();}</script>
  * @package jkOptionsFramework
  * @author gogobody
  * @version 1.0.0
