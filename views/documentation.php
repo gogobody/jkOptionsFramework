a2fa0e2643e9c67825ea5ba3fc1f28cf97f79a75
gogobody <138950128@qq.com>
2022-06-21 19:56:21 +0800

    update to 1.0.3

--- views/documentation.php
+++
@@ -1,5 +0,0 @@
-<?php if ( ! defined( 'ABSPATH' ) ) { die; } // Cannot access directly. ?>
-
-<p>If you are new to Codestar Framework, here is a step-by-step plan for getting started.</p>
-
-<p><a href="https://www.wolai.com/xvse6BfZ5vMxd9LgjYBjRM" class="button" target="_blank" rel="nofollow"><i class="fas fa-book"></i> Online Documentation</a></p>
